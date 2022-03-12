aaa4c87ba0de349fcac71ea99665c0fd3eb029c2
hop- <hovhannespalyan@gmail.com>
2022-03-12 21:38:51 +0400

    Add array processing in pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type Pipeline from './pipeline';
+import type { Pipeline } from './pipeline';
 import type * as Modules from './modules';
 
 export type {
--- src/pipeline/index.ts
+++ src/pipeline/index.ts
@@ -1,16 +1,32 @@
-import type Base from '../modules/base';
+import * as Bluebird from 'bluebird';
+import type { Base as BaseModule } from '../modules';
 
-export default class Pipeline {
-  pipe: Base[];
+export type Pipe = BaseModule | BaseModule[];
+export class Pipeline {
+  pipe: Pipe[];
 
-  constructor(pipe: Base[]) {
+  constructor(pipe: Pipe[]) {
     this.pipe = pipe;
   }
 
-  async execute(): Promise<void> {
-    let object = {};
-    this.pipe.forEach(async (m) => {
-      object = await m.run(object);
+  // run the pipeline
+  async run() {
+    let data: any;
+
+    this.pipe.forEach(async (p) => {
+      if (p instanceof Array) {
+        data = await Bluebird.map(data, (obj: any) => {
+          let d = obj;
+
+          p.forEach((m: any) => {
+            d = m.run(d);
+          });
+
+          return d;
+        });
+      } else {
+        data = await p.run(data);
+      }
     });
   }
 }

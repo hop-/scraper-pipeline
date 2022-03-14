a189f7015457965d4dcc1fbd2f15b0b8e6d5b556
hop- <hovhannespalyan@gmail.com>
2022-03-14 23:32:00 +0400

    Modify the logic of Pipeline.run

--- src/pipeline/index.ts
+++ src/pipeline/index.ts
@@ -1,32 +1,38 @@
 import * as Bluebird from 'bluebird';
 import type { Base as BaseModule } from '../modules';
 
-export type Pipe = BaseModule | BaseModule[];
-export class Pipeline {
-  pipe: Pipe[];
+export type Pipe = BaseModule | Pipe[];
 
-  constructor(pipe: Pipe[]) {
+async function process(data: any, pipe: Pipe): Promise<any> {
+  if (pipe instanceof Array) {
+    const dataArray = data instanceof Array ? data : [data];
+
+    return Bluebird.map(dataArray, async (obj: any) => {
+      let d = obj;
+      await Bluebird.each(pipe, async (p: Pipe) => {
+        d = await process(d, p);
+      });
+
+      return d;
+    });
+  }
+
+  return pipe.run(data);
+}
+export class Pipeline {
+  pipe: Pipe;
+
+  constructor(pipe: Pipe) {
     this.pipe = pipe;
   }
 
   // run the pipeline
-  async run() {
-    let data: any;
+  async run(): Promise<void> {
+    const emptyData = {
+      current: null,
+      previous: [],
+    };
 
-    this.pipe.forEach(async (p) => {
-      if (p instanceof Array) {
-        data = await Bluebird.map(data, (obj: any) => {
-          let d = obj;
-
-          p.forEach((m: any) => {
-            d = m.run(d);
-          });
-
-          return d;
-        });
-      } else {
-        data = await p.run(data);
-      }
-    });
+    await process(emptyData, this.pipe);
   }
 }
